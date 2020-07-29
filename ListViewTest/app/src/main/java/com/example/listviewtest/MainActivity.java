a90558e3ed349f25873a66e24719776f6fc3968d
linaiwang <linw0701@thundersoft.com>
2020-07-29 16:07:38 +0800

    没啥我就试试

---
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.listviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] data={"Apple","banana","Apple","banana","Apple","banana","Apple","banana","Apple","banana","Apple","banana","Apple","banana","Apple","banana","Apple","banana","Apple","banana"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        ListView listView=(ListView)findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+}
